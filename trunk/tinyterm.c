d2e623383b5debd10714ba4b96da198c317a424a
Seb_Linke@web.de <Seb_Linke@web.de@6b70c55c-77a0-11de-a249-6bf219df57d5>
2009-07-23 17:31:47 +0000

    initial import

---
+++ trunk/tinyterm.c
@@ -0,0 +1,78 @@
+/*
+ * Copyright (C) 2009 Sebastian Linke
+ *
+ * This is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Library General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+ 
+#include <vte/vte.h>
+#include "config.h"
+
+int
+main (int argc, char *argv[])
+{
+    GtkWidget *window, *terminal, *scrollbar, *design;
+    GError *icon_error;
+    GdkPixbuf *icon;
+    GdkGeometry geo_hints;
+
+    /* Init gtk and all widgets */
+    gtk_init (&argc, &argv);
+    window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+    terminal = vte_terminal_new ();
+    scrollbar = gtk_vscrollbar_new (VTE_TERMINAL (terminal)->adjustment);
+    design = gtk_hbox_new (FALSE, 0);
+
+    /* Set window icon */
+    icon_error = NULL;
+    icon = gdk_pixbuf_new_from_file (TINYTERM_ICON_PATH, &icon_error);
+    if (!icon) {
+        g_warning ("%s\n", icon_error->message);
+        g_error_free (icon_error);
+    }
+    gtk_window_set_icon (GTK_WINDOW (window), icon);
+
+    /* Set window title */
+    gtk_window_set_title (GTK_WINDOW (window), "TinyTerm");
+
+    /* Apply geometry hints to handle terminal resizing */
+    geo_hints.base_width = VTE_TERMINAL (terminal)->char_width;
+    geo_hints.base_height = VTE_TERMINAL (terminal)->char_height;
+    geo_hints.min_width = VTE_TERMINAL (terminal)->char_width;
+    geo_hints.min_height = VTE_TERMINAL (terminal)->char_height;
+    geo_hints.width_inc = VTE_TERMINAL (terminal)->char_width;
+    geo_hints.height_inc = VTE_TERMINAL (terminal)->char_height;
+    gtk_window_set_geometry_hints (GTK_WINDOW (window), terminal, &geo_hints,
+                                   GDK_HINT_RESIZE_INC | GDK_HINT_MIN_SIZE |
+                                   GDK_HINT_BASE_SIZE);
+
+    /* Open a shell */
+    vte_terminal_fork_command (VTE_TERMINAL (terminal), NULL, NULL, NULL, NULL,
+                               FALSE, FALSE, FALSE);
+
+    /* Quit when closing the window */
+    g_signal_connect (window, "delete-event", gtk_main_quit, NULL);
+
+    /* Quit when closing the shell (e.g. with Ctrl+D) */    
+    g_signal_connect (terminal, "child-exited", gtk_main_quit, NULL);
+
+    /* Put all widgets together and show the result */
+    gtk_box_pack_start (GTK_BOX (design), terminal, TRUE, TRUE, 0);
+    gtk_box_pack_start (GTK_BOX (design), scrollbar, FALSE, FALSE, 0);
+    gtk_container_add (GTK_CONTAINER (window), design);
+    gtk_widget_show_all (window);
+    gtk_main ();
+
+    return 0;
+}
